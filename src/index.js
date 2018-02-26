15648138cfe87b8c5c3caa5b67c031214e224519
Marina <35458264+marishahv@users.noreply.github.com>
2018-02-26 16:34:20 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,50 @@
 class Sorter {
+
   constructor() {
-    // your implementation
+    this.array = [];
+    this.comparator;
   }
 
   add(element) {
-    // your implementation
+    this.array.push(element);
   }
 
   at(index) {
-    // your implementation
+    return this.array[index];
   }
 
   get length() {
-    // your implementation
+    return this.array.length;
   }
 
   toArray() {
-    // your implementation
+    return this.array;
   }
 
-  sort(indices) {
-    // your implementation
+  sort(indices) {   
+    var tempArr = [];
+
+    indices.forEach(element => {
+      tempArr.push(this.array[element]);
+    });
+
+    if(typeof this.comparator === "function"){
+      tempArr.sort(this.comparator);          
+    }
+    else{
+      tempArr.sort((a, b) => a - b);          
+    }
+
+    indices.sort((a, b) => a - b);
+
+    for(var i = 0; i < tempArr.length; i++){
+      var j = indices[i];
+      this.array[j] = tempArr[i];
+    }   
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.comparator = compareFunction;  
   }
 }
 
